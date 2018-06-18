80ceaef0cb57498e18b6028604966be188b8aab3
Max Jöhnk <maxjoehnk@gmail.com>
2018-06-18 21:03:04 +0200

    feat(sync): use named thread

--- src/sync.rs
+++ src/sync.rs
@@ -1,21 +1,26 @@
+use failure::Error;
+use Rustic;
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-use super::Rustic;
-use std::sync::Arc;
 
-pub fn start(app: Arc<Rustic>) -> thread::JoinHandle<()> {
-    thread::spawn(move|| {
-        loop {
-            let providers = app.providers.clone();
-            for provider in providers {
-                let mut provider = provider.write().unwrap();
-                info!("Syncing {} library", provider.title());
-                match provider.sync(Arc::clone(&app.library)) {
-                    Ok(result) => info!("Synced {} tracks, {} albums, {} artist and {} playlists from {}", result.tracks, result.albums, result.artists, result.playlists, provider.title()),
-                    Err(err) => error!("Error syncing {}: {:?}", provider.title(), err)
+pub fn start(app: Arc<Rustic>) -> Result<thread::JoinHandle<()>, Error> {
+    let handle = thread::Builder::new()
+        .name("Background Sync".into())
+        .spawn(move || {
+            loop {
+                let providers = app.providers.clone();
+                for provider in providers {
+                    let mut provider = provider.write().unwrap();
+                    info!("Syncing {} library", provider.title());
+                    match provider.sync(Arc::clone(&app.library)) {
+                        Ok(result) => info!("Synced {} tracks, {} albums, {} artist and {} playlists from {}", result.tracks, result.albums, result.artists, result.playlists, provider.title()),
+                        Err(err) => error!("Error syncing {}: {:?}", provider.title(), err)
+                    }
                 }
+                thread::sleep(Duration::from_secs(5 * 60));
             }
-            thread::sleep(Duration::from_secs(5 * 60));
-        }
-    })
+        })?;
+
+    Ok(handle)
 }
