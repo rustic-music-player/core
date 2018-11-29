f7839823959094a54df1fcc8db3007fb47df1acf
Max Jöhnk <maxjoehnk@gmail.com>
2018-11-29 18:52:33 +0100

    change player trait to expose only immutable apis

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -12,50 +12,50 @@ pub use self::state::PlayerState;
 
 pub trait PlayerBackend: Send + Sync {
     /// Put a single track at the end of the current queue
-    fn enqueue(&mut self, track: &Track);
+    fn queue_single(&self, track: &Track);
 
     /// Put multiple tracks at the end of the current queue
-    fn enqueue_multiple(&mut self, tracks: &[Track]);
+    fn queue_multiple(&self, tracks: &[Track]);
 
     /// Queue single track behind the current
-    fn play_next(&mut self, track: &Track);
+    fn queue_next(&self, track: &Track);
 
     /// Returns all tracks which are queued up right now
-    fn queue(&self) -> Vec<Track>;
+    fn get_queue(&self) -> Vec<Track>;
 
     /// Clear the current queue
     /// Does not stop playback
-    fn clear_queue(&mut self);
+    fn clear_queue(&self);
 
     /// Returns the currently playing track or None when nothing is playing
     fn current(&self) -> Option<Track>;
 
     /// Play the previous track in the current queue
-    fn prev(&mut self) -> Result<Option<()>, Error>;
+    fn prev(&self) -> Result<Option<()>, Error>;
 
     /// Play the next track in the current queue
-    fn next(&mut self) -> Result<Option<()>, Error>;
+    fn next(&self) -> Result<Option<()>, Error>;
 
     /// Set the player state
-    fn set_state(&mut self, state: PlayerState) -> Result<(), Error>;
+    fn set_state(&self, state: PlayerState) -> Result<(), Error>;
 
     /// Get the player state
     fn state(&self) -> PlayerState;
 
     /// Set the volume of this player
-    fn set_volume(&mut self, volume: f32) -> Result<(), Error>;
+    fn set_volume(&self, volume: f32) -> Result<(), Error>;
 
     /// Get the volume of this player
     fn volume(&self) -> f32;
 
     /// Set time from the end of the current track when the next track should start playing
-    fn set_blend_time(&mut self, duration: Duration) -> Result<(), Error>;
+    fn set_blend_time(&self, duration: Duration) -> Result<(), Error>;
 
     /// Get time from the end of the current track when the next track should start playing
     fn blend_time(&self) -> Duration;
 
     /// Seek to a point in the current track
-    fn seek(&mut self, duration: Duration) -> Result<(), Error>;
+    fn seek(&self, duration: Duration) -> Result<(), Error>;
 
     fn observe(&self) -> Receiver<PlayerEvent>;
 
